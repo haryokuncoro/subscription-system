340bf5f26abff9959a5b20926ddfd140e275e0c8
haryo <emadekuncoro@gmail.com>
2026-06-28 13:34:01 +0700

    init project

---
+++ src/main/java/com/haryokuncoro/subscription_app/SubscriptionAppApplication.java
@@ -0,0 +1,13 @@
+package com.haryokuncoro.subscription_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionAppApplication.class, args);
+	}
+
+}
